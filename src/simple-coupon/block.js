19b9c08563af66abec065b12deb950de2c4d0f86
Zac Sanders <37851511+endurain@users.noreply.github.com>
2020-01-22 15:08:19 -0700

    Update block.js

--- src/simple-coupon/block.js
+++ src/simple-coupon/block.js
@@ -14,6 +14,11 @@
    icon: 'tickets',
    category: 'common',
    attributes: {
+     topText: {
+       type: 'string',
+       source: 'html',
+       selector: 'p.simple__coupon__top__text'
+     },
      discount: {
        type: 'string',
        source: 'html',
@@ -23,6 +28,11 @@
        type: 'string',
        source: 'html',
        selector: 'h3.simple__coupon__upper__body'
+     },
+     monthText: {
+       type: 'string',
+       source: 'html',
+       selector: 'span.simple__coupon__month__text'
      },
      lowerBody: {
        type: 'string',
@@ -41,18 +51,29 @@
        <div className="container">
         <div className="simpleCouponEditorWrapper">
           <div className="simpleCouponContentContainer">
+              <PlainText
+                onChange={ content => setAttributes({ topText: content }) }
+                value={ attributes.topText }
+                placeholder="Top Paragraph Text"
+                className="simple__coupon__top__text"
+              />
              <PlainText
                onChange={ content => setAttributes({ discount: content }) }
                value={ attributes.discount }
                placeholder="Coupon Discount"
                className="simple__coupon__discount"
              />
-              <hr />
              <PlainText
                onChange={ content => setAttributes({ upperBody: content }) }
                value={ attributes.upperBody }
                placeholder="Upper coupon text"
                className="simple__coupon__upper__body"
+             />
+             <PlainText
+               onChange={ content => setAttributes({ monthText: content }) }
+               value={ attributes.monthText }
+               placeholder="Per Month Text"
+               className="simple__coupon__month__text"
              />
              <PlainText
                onChange={ content => setAttributes({ lowerBody: content }) }
@@ -79,22 +100,37 @@
        <div className="simple__coupon__wrapper">
         <div className="simple__coupon__dashed">
            <div className="simple__coupon__content">
-             <h3 className="simple__coupon__discount">
-              { attributes.discount }
-             </h3>
-             <hr />
-             <h3 className="simple__coupon__upper__body">
-               { attributes.upperBody }
-             </h3>
-             <p className="simple__coupon__lower__body">
-             { attributes.lowerBody }
+             <p className="simple__coupon__top__text">
+             { attributes.topText }
              </p>
+             <div className="discount__inline">
+              <div className="content__left">
+                <h3 className="simple__coupon__upper__body">
+                  { attributes.upperBody }
+                </h3>
+                <p className="simple__coupon__lower__body">
+                { attributes.lowerBody }
+                </p>
+              </div>
+              <div className="content__right">
+                 <h3 className="simple__coupon__discount">
+                  { attributes.discount }
+                 </h3>
+                 <span className="simple__coupon__month__text">
+                  { attributes.monthText }
+                </span>
+              </div>
+             </div>
            </div>
            <div className="simple__coupon__bottom">
-            <img
-              src="/wp-content/uploads/2019/12/neerings_logo_header-01-1.png"
-              alt="Neerings Logo"
-            />
+            <div className="bottom__content__left">
+              <img
+                className="company__logo"
+                src="/wp-content/plugins/ensuite/img/TS_logo_anniversary.png"
+                alt="Tripleservice Logo"
+              />
+              <a href="tel:+18155393828">815-539-3828</a>
+            </div>
             <p className="simple__coupon__disclaimer">
               { attributes.disclaimer }
             </p>
